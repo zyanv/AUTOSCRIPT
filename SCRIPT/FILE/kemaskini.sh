47dc0e52ba61019a88414a5a02a0d5e166d30f0c
zyanv <azammuddinazrul18@gmail.com>
2026-03-07 01:16:08 +0800

    Update kemaskini.sh

--- SCRIPT/FILE/kemaskini.sh
+++ SCRIPT/FILE/kemaskini.sh
@@ -8,5 +8,7 @@ rm -f /usr/local/bin/menu
 # download script
 cd /usr/local/bin
 wget -O menu "${websc}/SCRIPT/FILE/menu.sh" && chmod +x menu
+wget -O mray "${websc}/SCRIPT/FILE/mray.sh" && chmod +x mray
+wget -O mssh "${websc}/SCRIPT/FILE/mssh.sh" && chmod +x mssh
 cd
 clear
