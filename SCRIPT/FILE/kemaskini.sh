cde71ab7033eea52867db749b430403471778bb8
zyanv <azammuddinazrul18@gmail.com>
2026-03-07 10:39:32 +0800

    Update kemaskini.sh

--- SCRIPT/FILE/kemaskini.sh
+++ SCRIPT/FILE/kemaskini.sh
@@ -4,13 +4,9 @@ clear
 websc=https://raw.githubusercontent.com/zyanv/AUTOSCRIPT/main
 
 #delete file
-rm -f /usr/local/bin/menu
-rm -f /usr/local/bin/mray
-rm -f /usr/local/bin/mssh
+rm -f /usr/local/bin/menu-tweak
 
 # download script
 cd /usr/local/bin
-wget -O menu "${websc}/SCRIPT/FILE/menu.sh" && chmod +x menu
-wget -q -O /usr/local/bin/mray "${websc}/SCRIPT/FILE/mray.sh" && chmod +x cd/usr/local/bin/mxray
-wget -q -O /usr/local/bin/mssh "${websc}/SCRIPT/FILE/mssh.sh" && chmod +x cd/usr/local/bin/mssh
+wget -O menu-tweak "${websc}/SCRIPT/FILE/menu-tweak.sh" && chmod +x menu-tweak
 clear
