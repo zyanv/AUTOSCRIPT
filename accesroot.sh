e84383032eade9a3edbec1c3866dfd0c8742abe0
zyanv <azammuddinazrul18@gmail.com>
2026-03-09 07:21:34 +0800

    Update and rename SCRIPT/FILE/aksesroot (1).sh to accesroot.sh

--- accesroot.sh
+++ accesroot.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # xixi
 # echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
-wget -q -O /tmp/sshd_config https://raw.githubusercontent.com/hokagelegend9999/alpha.v2/refs/heads/main/sshd_config && sudo mv /tmp/sshd_config /etc/ssh/sshd_config;
+wget -q -O /tmp/sshd_config https://raw.githubusercontent.com/zyanv/AUTOSCRIPT/main/SCRIPT/FILE/sshd_config && sudo mv /tmp/sshd_config /etc/ssh/sshd_config;
 systemctl restart ssh;
 clear;
-echo -e "Masukkan Password:";
+echo -e "Enter Password:";
 read -e pwe;
 usermod -p "$(perl -e "print crypt('$pwe', 'Q4')")" root;
 clear;
-printf "Mohon Simpan Informasi Akun VPS Ini
+printf "Save This Vps Information
 ============================================
-Akun Root (Akun Utama)
+Root Account (Main Account)
 Ip address = $(curl -Ls http://ipinfo.io/ip)
 Username   = root
 Password   = $pwe
