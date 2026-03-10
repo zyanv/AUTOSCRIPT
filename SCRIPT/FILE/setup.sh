2c77df305fd16d27994d706fc53cadbf7277ea70
zyanv <azammuddinazrul18@gmail.com>
2026-03-10 18:14:35 +0800

    Update setup.sh

--- SCRIPT/FILE/setup.sh
+++ SCRIPT/FILE/setup.sh
@@ -157,6 +157,15 @@ bash <(curl -s https://raw.githubusercontent.com/zyanv/AUTOSCRIPT/main/SCRIPT/FI
 sleep 2
 clear
 
+#install nginx
+echo -e "============================================="
+echo -e " ${green} INSTALLING NGINX  ${NC}"
+echo -e "============================================="
+sleep 2
+bash <(curl -s https://raw.githubusercontent.com/zyanv/AUTOSCRIPT/main/SCRIPT/FILE/nginx.sh)
+sleep 2
+clear
+
 #install xcore changer
 echo -e "============================================="
 echo -e " ${green} INSTALLING XRAY CHANGER  ${NC}"
