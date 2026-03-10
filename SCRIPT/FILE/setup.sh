f6eacf93c8d28c47f5f81181eeadf8095c9e1506
zyanv <azammuddinazrul18@gmail.com>
2026-03-10 16:41:15 +0800

    Update setup.sh

--- SCRIPT/FILE/setup.sh
+++ SCRIPT/FILE/setup.sh
@@ -160,6 +160,71 @@ echo -e " ${red}RESOLVE INSTALL DONE ${NC}"
 sleep 2
 clear
 
+# Create the directory for iptables rules if it doesn't exist
+mkdir -p /etc/iptables
+
+# Flush existing iptables rules
+iptables -F
+iptables -X
+
+# Open essential ports
+iptables -I INPUT -p tcp --dport 22 -j ACCEPT
+iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+iptables -I INPUT -p tcp --dport 443 -j ACCEPT
+iptables -I INPUT -p tcp --dport 8880 -j ACCEPT
+iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
+
+# Save iptables rules
+mkdir -p /etc/iptables
+iptables-save > /etc/iptables/rules.v4
+
+# Create a systemd service file to load iptables rules at boot
+cat <<EOT > /etc/systemd/system/iptables-restore.service
+[Unit]
+Description=Restore iptables firewall rules
+Before=network-pre.target
+Wants=network-pre.target
+DefaultDependencies=no
+
+[Service]
+Type=oneshot
+ExecStart=/sbin/iptables-restore < /etc/iptables/rules.v4
+ExecReload=/sbin/iptables-restore < /etc/iptables/rules.v4
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+# // Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+
+clear
+
 rm -f /root/ssh-vpn.sh
 rm -f /root/install-xray.sh
 rm -f /root/websocket.sh
