db3249082006b510392e32d136807122aa3bd2d2
zyanv <azammuddinazrul18@gmail.com>
2026-03-25 15:09:45 +0800

    Update kemaskini.sh

--- SCRIPT/FILE/kemaskini.sh
+++ SCRIPT/FILE/kemaskini.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 clear
 
-websc=https://raw.githubusercontent.com/JinGGoVPN/DATA/main
+websc=https://raw.githubusercontent.com/zyanv/AUTOSCRIPT/main
 
 #delete file
 rm -f /usr/local/bin/add-xvless
-rm -f /usr/local/bin/trial-xvless
 # download script
 cd /usr/local/bin
-wget -O add-xvless "${websc}/script/lifetime/upgrade/add-xvless.sh" && chmod +x add-xvless
-wget -O trial-xvless "${websc}/script/lifetime/upgrade/trial-xvless.sh" && chmod +x trial-xvless
+wget -O add-xvless "${websc}/SCRIPT/FILE/add-xvless.sh" && chmod +x add-xvless
 cd
 clear
